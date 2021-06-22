cee34a2fabdf95b9c8517f5272fbae27254fafa6
souzavini <viniciuscarr@hotmail.com>
2021-06-22 19:56:23 -0300

    Implementacao Stripe e comunicacao com checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import {useSession,signIn} from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps{
@@ -5,8 +8,36 @@ interface SubscribeButtonProps{
 }
 
 export function SubscribeButton({priceId} : SubscribeButtonProps){
+
+    const [session] = useSession();
+
+    async function handleSubscribe(){
+        if(!session){
+            signIn('github')
+            return;
+        }
+
+        //criação checkout session
+        try {
+
+            const response = await api.post('/subscribe')
+            console.log(response);
+ 
+            const {sessionId} = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId: sessionId});
+
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     return(
-        <button type="button" className={styles.subscribeButton}>
+        <button type="button" 
+        className={styles.subscribeButton}
+        onClick={handleSubscribe}>
             Subscribe Button
         </button>
     )
